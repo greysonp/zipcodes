a63d3e2b78f881d088dcf8d62876244f39134ba7
Greyson Parrelli <greyson@signal.org>
2020-09-30 17:08:00 -0400

    Properly parse lat/long as floats.

--- build.js
+++ build.js
@@ -10,8 +10,8 @@ lines = lines.splice(1, lines.length)
 lines.forEach(line => {
   let parts = line.split(',')
   let zip   = parts[0].trim()
-  let lat   = parts[1].trim()
-  let long  = parts[2].trim()
+  let lat   = parseFloat(parts[1].trim())
+  let long  = parseFloat(parts[2].trim())
 
   emptyDirSync(`docs/${zip}`)
   Deno.writeTextFileSync(`docs/${zip}/index.json`, JSON.stringify({ lat: lat, long: long }, null, 2))
